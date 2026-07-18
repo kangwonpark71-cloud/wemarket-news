5c1494c30f6db17b89450f862c85b488d90b1dda
Park KW <kwpark0047@gmail.com>
2026-07-18 20:37:09 +0900

    fix: make startAllSchedulers import dynamic in instrumentation to prevent edge compilation of playwright

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,6 @@
-import { startAllSchedulers } from '@/lib/startup/schedulers'
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { startAllSchedulers } = await import('@/lib/startup/schedulers')
     await startAllSchedulers()
     console.log('[Instrumentation] All schedulers started')
   }
